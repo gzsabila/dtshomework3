7dfce6ded853bc274b5d2066ce5872a0fc171090
gzsabila <gzsabila@gmail.com>
2021-08-23 20:53:23 +0700

    modified attributes

--- main.js
+++ main.js
@@ -5,28 +5,28 @@ function select() {
 
   const calculate = new Convert();
   function calc() {
-    if (document.getElementById("from").value == "cel") {
+    if (document.getElementById("from").value == "cel") { // dari celcius
       const res = calculate.fromCelcius(
         document.getElementById("to").value,
         parseInt(document.getElementById("nilai_in").value)
       );
       document.getElementById("nilai_out").value = res;
     }
-    if (document.getElementById("from").value == "rea") {
+    if (document.getElementById("from").value == "rea") { //dari reamur
       const res = calculate.fromReamur(
         document.getElementById("to").value,
         parseInt(document.getElementById("nilai_in").value)
       );
       document.getElementById("nilai_out").value = res;
     }
-    if (document.getElementById("from").value == "fahr") {
+    if (document.getElementById("from").value == "fahr") { //dari fahrenheit
       const res = calculate.fromFahrenheit(
         document.getElementById("to").value,
         parseInt(document.getElementById("nilai_in").value)
       );
       document.getElementById("nilai_out").value = res;
     }
-    if (document.getElementById("from").value == "kel") {
+    if (document.getElementById("from").value == "kel") { //dari kelvin
         const res = calculate.fromKelvin(
             document.getElementById("to").value,
             parseInt(document.getElementById("nilai_in").value)
